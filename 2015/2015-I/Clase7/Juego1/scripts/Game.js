d8cb0a0f790812b38dacffbfdc0b8f28f9ae9be5
LUis <valdivialuis1989@gmail.com>
2015-05-09 09:24:21 -0500

    Clase7

---
+++ 2015/2015-I/Clase7/Juego1/scripts/Game.js
@@ -0,0 +1,60 @@
+Game = function(game){
+	this.bg = null;
+	this.hero = null;
+	this.timeCreateCandy = 0;
+	this.candyGroup = null;
+}
+
+Game.prototype = {
+	create:function(){
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+		this.layer = this.add.group();
+		this.hero = this.add.sprite(0,0,'hero');
+		this.hero.animations.add('flying',[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]);
+		this.hero.play('flying',20,true);
+		this.hero.anchor.setTo(0.5,0.5);
+		this.hero.x = this.hero.width/2;
+		this.hero.y = this.hero.height/2;
+		this.candyGroup = this.add.group();
+
+		this.physics.enable(this.hero, Phaser.Physics.Phaser);
+		//this.hero.body.immovable  = true;
+	},
+	update:function(){
+		this.hero.y = this.input.y;
+		if(this.hero.y<this.hero.height/2){
+			this.hero.y = this.hero.height/2;
+		}
+		if(this.hero.y>Global.HEIGHT-(this.hero.height/2)){
+			this.hero.y=Global.HEIGHT-(this.hero.height/2)
+		}
+		this.timeCreateCandy += this.time.elapsed;
+		
+		if(this.timeCreateCandy>1000){
+			this.timeCreateCandy = 0;
+			var candy = this.add.sprite(0,0,'candy');
+			candy.frame = this.rnd.integerInRange(1,5)
+			candy.x = Global.WIDTH;
+			candy.y =  this.rnd.integerInRange(candy.height/2, Global.WIDTH-candy.height/2);
+			this.physics.enable(candy, Phaser.Physics.ARCADE);
+			candy.checkWorldBounds  = true;
+			candy.body.velocity.x = -100;
+			candy.outOfBoundsKill = true;
+			this.candyGroup.add(candy);
+			candy.anchor.setTo(0.5,0.5);
+			candy.rotateMe = (Math.random()*4)-2;
+			candy.events.onOutOfBounds.add(this.removeCandy, this);
+		}
+		this.physics.arcade.collide(this.hero,this.candyGroup,this.collisionHandler,null,this);
+		this.candyGroup.forEach(function(candy){
+			candy.angle += candy.rotateMe;
+		});
+	},
+	collisionHandler:function(sp1,sp2){
+		sp2.kill();
+	},
+	removeCandy:function(sprite){
+		sprite.destroy();	
+	}
+}
+
---
+++ 2015/2015-I/Clase7/Juego1/scripts/Preloader.js
@@ -0,0 +1,31 @@
+Preloader = function(game){
+	this.loadingBar = null;
+	Global = {WIDTH:1024,HEIGHT:768};
+}
+
+Preloader.prototype = {
+	preload:function(){
+		this.loadingBar = this.add.sprite(0,0,'loading');
+		this.loadingBar.anchor.setTo(0.5,0.5);
+		this.loadingBar.x = Global.WIDTH/2;
+		this.loadingBar.y = Global.HEIGHT/2;
+		this.load.setPreloadSprite(this.loadingBar);
+
+		this.load.audio('game','sounds/bgGame.mp3');
+		this.load.image('bgLayer1','img/bgLayer1.png');
+		this.load.image('bgLayer2','img/bgLayer2.png');
+		this.load.image('bgLayer3','img/bgLayer3.png');
+		this.load.image('bgLayer4','img/bgLayer4.png');
+		this.load.image('bgWelcome','img/bgWelcome.jpg');
+		this.load.spritesheet('candy','img/candy.png',82,98);
+		this.load.spritesheet('hero','img/hero.png',155,77);
+		this.load.image('welcome_aboutButton','img/welcome_aboutButton.png');
+		this.load.image('welcome_hero','img/welcome_hero.png');
+		this.load.image('welcome_playButton','img/welcome_playButton.png');
+		this.load.image('welcome_title','img/welcome_title.png');
+		
+	},
+	create:function(){
+		this.state.start('Menu');
+	}
+}
