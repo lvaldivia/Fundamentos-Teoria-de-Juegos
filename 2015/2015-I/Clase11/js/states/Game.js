42ec9529b04bd055f951ee3b77787d03a2fd5a8b
LUis <valdivialuis1989@gmail.com>
2015-06-06 13:05:31 -0500

    juego

--- 2015/2015-I/Clase11/js/states/Game.js
+++ 2015/2015-I/Clase11/js/states/Game.js
@@ -18,7 +18,7 @@ Game.prototype = {
   	this.player.play('running');
   	this.physics.startSystem(Phaser.Physics.ARCADE);
 
-  	this.physics.arcade.gravity.y = 500;
+  	this.physics.arcade.gravity.y = 1000;
 
   	this.physics.arcade.enable(this.player);
   	this.player.body.setSize(38,60,0,0);
@@ -31,39 +31,134 @@ Game.prototype = {
 
   	this.cursors = this.input.keyboard.createCursorKeys();
 
-  	this.currentPlatform = new Platform(
-  			this.game,12,0,200,-this.worldSpeed);
+  	this.floorPool = this.add.group();
+  	this.platformPool = this.add.group();
 
-  	/*this.platform = this.add.sprite(40,250,'floor');
-  	this.physics.arcade.enable(this.platform);
-  	this.platform.body.allowGravity = false;
-  	this.platform.body.immovable = true;*/
+  	this.currentPlatform = new Platform(
+  			this.game,this.floorPool,12,0,200,-this.worldSpeed);
+
+  	this.platformPool.add(this.currentPlatform);
 
   },
   update:function(){
-    this.physics.arcade.collide(this.player,
-    					this.currentPlatform);
 
-    if(this.player.body.touching.down){
-    	this.player.body.velocity.x = this.worldSpeed;
-    }else{
-    	this.player.body.x = 0;
-    }
-    if(this.cursors.up.isDown || 
-    	this.game.input.activePointer.isDown){
-    	this.playerJump();
-    }else if(this.cursors.up.isUp ||
-    	this.game.input.activePointer.isUp
-    	){
-    	this.isJumping = false;
-    }
+  	if(this.player.alive){
+		this.platformPool.forEachAlive(function(platform, index){
+			this.game.physics.arcade.collide(this.player,platform);
+			if(platform.length && platform.children[platform.length-1].right<0){
+				platform.kill();
+			}
+		},this);
+
+	    if(this.player.body.touching.down){
+	    	this.player.body.velocity.x = this.worldSpeed;
+	    }else{
+	    	this.player.body.x = 0;
+	    }
+	    if(this.cursors.up.isDown || 
+	    	this.game.input.activePointer.isDown){
+	    	this.playerJump();
+	    }else if(this.cursors.up.isUp ||
+	    	this.game.input.activePointer.isUp
+	    	){
+	    	this.isJumping = false;
+	    }
+
+	    if(this.currentPlatform.length && 
+			this.currentPlatform.children[this.currentPlatform.length-1].right 
+				< this.game.world.width)
+	    	{
+				this.createPlatform();
+			}
+		if(this.player.top >= this.world.height){
+			this.gameOver();
+		}
+	}
+  },
+  gameOver:function(){
+  	this.player.kill();
+  	this.overlay = this.add.bitmapData(this.game.width, this.game.height);
+  	this.overlay.ctx.fillStyle = "#000";
+  	this.overlay.ctx.fillRect(0,0,this.game.width, this.game.height);
+
+  	this.panel = this.add.sprite(0,0,this.overlay);
+  	this.panel.alpha  =0.5;
+  	this.btn = this.add.button(this.game.width/2, this.game.height/2+100,
+  		'facebook_share',this.share,this);
+  	this.btn.anchor.setTo(0.5,0.5);
+  },
+  share:function(){
+  	FB.ui(
+    {
+      method: 'feed',
+      link: 'https://developers.facebook.com/docs/',
+      name: 'Mi gran demo',
+      caption: 'Esto es mi test',
+      description: 'Obtuve un puntaje de '
+    },function(response){});
+
   },
   playerJump:function(){
   	if(this.player.body.touching.down){
+  		this.startJumpY = this.player.y;
   		this.isJumping = true;
   		this.jumpPeaked = false;
   		this.player.body.velocity.y = -300;
+  	}else if(this.isJumping && !this.jumpPeaked){
+  		var distanceJumped = this.startJumpY-this.player.y;
+  		if(distanceJumped<= this.maxJumpDistance){
+  			this.player.body.velocity.y = -300;
+  		}else{
+  			this.jumpPeaked = true;
+  		}
   	}
+  },
+  loadLevel:function(){
+  	this.createPlatform();
+  },
+  createPlatform:function(){
+  	var nextPlatform = this.generateRandomPlatform();
+  	if(nextPlatform){
+  		this.currentPlatform = this.platformPool.getFirstDead();
+  		if(!this.currentPlatform){
+  			this.currentPlatform = new Platform(
+  				this.game,this.floorPool,nextPlatform.numTiles,
+  				this.game.world.width + nextPlatform.separation, 
+  				nextPlatform.y,
+  				-this.worldSpeed);
+  		}else{
+  			this.currentPlatform.prepare(nextPlatform.numTiles,
+  				this.game.world.width + nextPlatform.separation, 
+  				nextPlatform.y,
+  				-this.worldSpeed);
+
+  		}
+  		this.platformPool.add(this.currentPlatform);
+  	}
+  },
+  generateRandomPlatform:function(){
+  	var data = {};
+  	var minSeparation = 60;
+  	var maxSperation = 200;
+
+  	data.separation = minSeparation + 
+  						Math.random()*(maxSperation-minSeparation);
+  	var minDifY = -120;
+  	var maxDifY = 120;
+
+  	data.y = this.currentPlatform.children[0].y 
+  			+ minDifY+ Math.random()*(maxDifY-minDifY);
+  	data.y = Math.max(150,data.y);
+  	data.y = Math.min(this.world.height-50,data.y);
+
+  	var minTiles = 1;
+  	var maxTiles = 5;
+
+
+  	data.numTiles = minTiles + Math.random()*(maxTiles-minTiles);
+  	return data;
+
+
   },
   render:function(){
   	//this.game.debug.body(this.player);
